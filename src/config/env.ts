044a8f3335a8beaa9aa9719e015b7b0479c475bc
EMESIS78 <fabrismal351@gmail.com>
2025-06-21 03:16:47 -0500

    cambios para el app

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,3 @@
-// export const API_URL=https://taxi-estrella-back-production.up.railway.app
-export const API_URL='http://192.168.0.86:8080'
+export const API_URL='https://taxi-estrella-back-production.up.railway.app'
+//export const API_URL='http://192.168.0.86:8080'
 export const GOOGLE_API_KEY='AIzaSyCZqHbNMMzl9ODmkN1Hb8nqk8sFsOj6Arg'
